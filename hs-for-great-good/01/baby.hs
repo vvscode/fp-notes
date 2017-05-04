8bd3ef30a441455c5e6fb79b7b82d0dcffa211e1
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-05-05 01:43:05 +0300

    First functions

---
+++ hs-for-great-good/01/baby.hs
@@ -0,0 +1,10 @@
+doubleMe x = x * 2
+
+-- doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                      then x
+                      else x * 2
+
+doubleSmallNumberInline x = (if x > 100 then x else x*2) + 1
