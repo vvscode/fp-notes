4e6827dd10abaf63c7beaea22f720d5f93e87859
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-05-07 09:59:27 +0300

    Add isPolindrom

---
+++ hs-for-great-good/03/isPolindrom.hs
@@ -0,0 +1,12 @@
+slice from to xs = take (to - from + 1) (drop from xs)
+
+isPolindrom::[Char] -> Bool
+isPolindrom [] = True
+isPolindrom [x] = True
+isPolindrom xs =
+    let fst = head xs
+        lst = last xs
+        newXs = slice 1 ((length xs) - 2) xs
+    in if (fst == lst)
+    then (isPolindrom newXs)
+    else False
