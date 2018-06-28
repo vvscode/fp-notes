703d5a0454aca12acce60dd424503921f675cea0
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2018-06-28 10:48:46 +0300

    Use One Function Argument at a Time with Currying and Partial Application in PureScript

--- egghead-purescript/src/Functions.purs
+++ egghead-purescript/src/Functions.purs
@@ -2,5 +2,11 @@ module Functions where
 
 import Prelude
 
+-- addMe (a -> ( b -> (a + b)))
 addMe :: Int -> Int -> Int
-addMe x y = x + y
+-- addMe x y = x + y
+addMe = \a -> \b -> a + b
+
+-- addFive (b -> (a +b))
+addFive :: Int -> Int
+addFive = addMe 5
--- egghead-purescript/src/Main.purs
+++ egghead-purescript/src/Main.purs
@@ -9,6 +9,11 @@ import Functions
 main :: Effect Unit
 main = do
   log "Hello sailor!"
+
   log ""
   log "addMe 5 6"
   log $ show $ addMe 5 6
+  
+  log ""
+  log "addFive 4"
+  log $ show $ addFive 4
