850c2ac56c019d6bd6bf8b44c926b66930679484
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-07-13 21:24:18 +0400

    Aglebraic data type in PS and show instance

--- egghead-purescript/src/Functions.purs
+++ egghead-purescript/src/Functions.purs
@@ -59,3 +59,30 @@ length :: forall a. Array a -> Int
 --   else 1 + length(unsafePartial tail arr)
 length [] = 0
 length arr = 1 + length (unsafePartial tail arr)
+
+-- Aglebraic data type in PS and show instance
+data Vehicle
+  = Car Wheels
+  | Motorcycle Wheels
+  | Skateboard Wheels
+  | Bicycle Wheels
+  
+data Wheels = Wheels Int
+
+instance showWheels' :: Show Wheels where
+  show = showWheels
+  
+instance showVehicle' :: Show Vehicle where
+  show = showVehicle
+  
+myVehicle :: Vehicle
+myVehicle = Car (Wheels 6)
+
+showVehicle :: Vehicle -> String
+showVehicle (Car a) = "Vehicle: Car, " <> show a
+showVehicle (Motorcycle a) = "Vehicle: Motorcycle, " <> show a
+showVehicle (Skateboard a) = "Vehicle: Skateboard, " <> show a
+showVehicle (Bicycle a) = "Vehicle: Bicycle, " <> show a
+
+showWheels :: Wheels -> String
+showWheels (Wheels a) = "Wheels: " <> show a
